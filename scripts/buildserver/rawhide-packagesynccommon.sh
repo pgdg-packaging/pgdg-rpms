9079dbd881249e5f43d5cbe3ed83648d6edd585a
Devrim Gunduz <devrim@gunduz.org>
2025-03-13 23:57:04 +0300

    scripts: dnf-srpms and dnf-debuginfo use separate distribution IDs, so invent another variable for that

--- scripts/buildserver/rawhide-packagesynccommon.sh
+++ scripts/buildserver/rawhide-packagesynccommon.sh
@@ -49,10 +49,10 @@ rsync --checksum -ave ssh --delete $TESTING_COMMON_SRPM_DIR/ yumupload@yum.postg
 
 # Sync SRPMs to S3 bucket:
 aws s3 sync $TESTING_COMMON_SRPM_DIR s3://dnf-srpms.postgresql.org20250313103537584600000001/srpms/testing/$packageSyncVersion/$osdistro/$os-$osarch
-aws cloudfront create-invalidation --distribution-id $CF_DISTRO_ID --path /srpms/testing/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
+aws cloudfront create-invalidation --distribution-id $CF_SRPM_DISTRO_ID --path /srpms/testing/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
 
 # Sync debug* RPMs to S3 bucket:
 aws s3 sync $TESTING_COMMON_DEBUG_RPM_DIR s3://dnf-debuginfo.postgresql.org20250312201116649700000001/testing/debug/common/$osdistro/$os-$osarch/
-aws cloudfront create-invalidation --distribution-id $CF_DISTRO_ID --path /testing/debug/common/$osdistro/$os-$osarch/repodata/*
+aws cloudfront create-invalidation --distribution-id $CF_DEBUG_DISTRO_ID --path /testing/debug/common/$osdistro/$os-$osarch/repodata/*
 
 exit 0
