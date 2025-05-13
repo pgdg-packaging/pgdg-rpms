pgdg-packaging/pgdg-rpms

commit c041426cd57aa2b5c5732e8ff5d881420f413e30

PostgreSQL 18, 17, 16, 15, 14 and 13: Remove explicit calls to CLANG and LLVM_CONFIG on RHEL 8+, Fedora and SLES as they are the same across all distros (and also llvm-config-64 is removed from Fedora 42). So this commit basically enables builds on Fedora 42

rpm/redhat/main/non-common/postgresql-16/main/postgresql-16.spec
@@ -58,9 +58,9 @@ Version:	16.8
 %if 0%{?suse_version} >= 1315
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	420002PGDG%{?dist}
+Release:	420003PGDG%{?dist}
 %else
-Release:	2PGDG%{?dist}
+Release:	3PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -477,13 +477,6 @@ LDFLAGS="-Wl,--as-needed"; export LDFLAGS
 %endif
 
 export CFLAGS
-
-%if 0%{?fedora} >= 37 || 0%{?rhel} >= 8
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config-64
-%endif
-%if 0%{?suse_version} >= 1315
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
-%endif
 
 # These configure options must match main build
 ./configure --enable-rpath \
@@ -1234,6 +1227,11 @@ fi
 %endif
 
 %changelog
+* Mon Mar 24 2025 Devrim Gunduz <devrim@gunduz.org> - 16.8-3PGDG
+- Remove explicit calls to CLANG and LLVM_CONFIG as they are the
+  same across all distros (and also llvm-config-64 is removed from
+  Fedora 42).
+
 * Fri Mar 07 2025 Devrim Gunduz <devrim@gunduz.org> - 16.8-2PGDG
 - Remove redundant BR
 

rpm/redhat/main/non-common/postgresql-17/main/postgresql-17.spec
@@ -47,9 +47,9 @@ Version:	17.4
 %if 0%{?suse_version} >= 1500
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	420002PGDG%{?dist}
+Release:	420003PGDG%{?dist}
 %else
-Release:	2PGDG%{?dist}
+Release:	3PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -453,15 +453,7 @@ CFLAGS=`echo $CFLAGS|xargs -n 1|grep -v ffast-math|xargs -n 100`
 %if 0%{?rhel}
 LDFLAGS="-Wl,--as-needed"; export LDFLAGS
 %endif
-
 export CFLAGS
-
-%if 0%{?fedora} || 0%{?rhel}
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config-64
-%endif
-%if 0%{?suse_version} >= 1500
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
-%endif
 
 # These configure options must match main build
 ./configure --enable-rpath \
@@ -1226,6 +1218,11 @@ fi
 %endif
 
 %changelog
+* Mon Mar 24 2025 Devrim Gunduz <devrim@gunduz.org> - 17.4-3PGDG
+- Remove explicit calls to CLANG and LLVM_CONFIG as they are the
+  same across all distros (and also llvm-config-64 is removed from
+  Fedora 42).
+
 * Fri Mar 07 2025 Devrim Gunduz <devrim@gunduz.org> - 17.4-2PGDG
 - Remove redundant BR
 

rpm/redhat/main/non-common/postgresql-18/main/postgresql-18.spec
@@ -458,13 +458,6 @@ LDFLAGS="-Wl,--as-needed"; export LDFLAGS
 
 export CFLAGS
 
-%if 0%{?fedora} || 0%{?rhel}
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config-64
-%endif
-%if 0%{?suse_version} >= 1500
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
-%endif
-
 # These configure options must match main build
 ./configure --enable-rpath \
 	--prefix=%{pgbaseinstdir} \

rpm/redhat/main/non-common/postgresql-13/main/postgresql-13.spec
@@ -76,9 +76,9 @@ Version:	13.20
 %if 0%{?suse_version} >= 1315
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	420002PGDG%{?dist}
+Release:	420003PGDG%{?dist}
 %else
-Release:	2PGDG%{?dist}
+Release:	3PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -588,10 +588,7 @@ export PYTHON=/usr/bin/python3
 	export CLANG=/opt/rh/llvm-toolset-7/root/usr/bin/clang LLVM_CONFIG=%{_libdir}/llvm5.0/bin/llvm-config
 %endif
 %endif
-%if 0%{?fedora} || 0%{?rhel} >= 8
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config-64
-%endif
-%if 0%{?suse_version} >= 1315
+%if 0%{?fedora} || 0%{?rhel} >= 8 || 0%{?suse_version} >= 1315
 export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
 %endif
 
@@ -1345,6 +1342,11 @@ fi
 %endif
 
 %changelog
+* Mon Mar 24 2025 Devrim Gunduz <devrim@gunduz.org> - 13.20-3PGDG
+- Remove explicit calls to CLANG and LLVM_CONFIG on RHEL 8+, Fedora
+  and SLES as they are the same across all distros (and also
+  llvm-config-64 is removed from Fedora 42).
+
 * Fri Mar 07 2025 Devrim Gunduz <devrim@gunduz.org> - 13.20-2PGDG
 - Remove redundant BR
 

rpm/redhat/main/non-common/postgresql-14/main/postgresql-14.spec
@@ -80,9 +80,9 @@ Version:	14.17
 %if 0%{?suse_version} >= 1315
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	420002PGDG%{?dist}
+Release:	420003PGDG%{?dist}
 %else
-Release:	2PGDG%{?dist}
+Release:	3PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -604,10 +604,7 @@ export PYTHON=/usr/bin/python3
 	export CLANG=/opt/rh/llvm-toolset-7/root/usr/bin/clang LLVM_CONFIG=%{_libdir}/llvm5.0/bin/llvm-config
 %endif
 %endif
-%if 0%{?fedora} || 0%{?rhel} >= 8
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config-64
-%endif
-%if 0%{?suse_version} >= 1315
+%if 0%{?fedora} || 0%{?rhel} >= 8 || 0%{?suse_version} >= 1315
 export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
 %endif
 
@@ -1382,6 +1379,11 @@ fi
 %endif
 
 %changelog
+* Mon Mar 24 2025 Devrim Gunduz <devrim@gunduz.org> - 14.17-3PGDG
+- Remove explicit calls to CLANG and LLVM_CONFIG on RHEL 8+, Fedora
+  and SLES as they are the same across all distros (and also
+  llvm-config-64 is removed from Fedora 42).
+
 * Fri Mar 07 2025 Devrim Gunduz <devrim@gunduz.org> - 14.17-2PGDG
 - Remove redundant BR
 

rpm/redhat/main/non-common/postgresql-15/main/postgresql-15.spec
@@ -80,9 +80,9 @@ Version:	15.12
 %if 0%{?suse_version} >= 1315
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	420002PGDG%{?dist}
+Release:	420003PGDG%{?dist}
 %else
-Release:	2PGDG%{?dist}
+Release:	3PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -612,10 +612,7 @@ export PYTHON=/usr/bin/python3
 	export CLANG=/opt/rh/llvm-toolset-7/root/usr/bin/clang LLVM_CONFIG=%{_libdir}/llvm5.0/bin/llvm-config
 %endif
 %endif
-%if 0%{?fedora} || 0%{?rhel} >= 8
-export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config-64
-%endif
-%if 0%{?suse_version} >= 1315
+%if 0%{?fedora} || 0%{?rhel} >= 8 || 0%{?suse_version} >= 1315
 export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
 %endif
 
@@ -1391,6 +1388,11 @@ fi
 %endif
 
 %changelog
+* Mon Mar 24 2025 Devrim Gunduz <devrim@gunduz.org> - 15.12-3PGDG
+- Remove explicit calls to CLANG and LLVM_CONFIG on RHEL 8+, Fedora
+  and SLES as they are the same across all distros (and also
+  llvm-config-64 is removed from Fedora 42).
+
 * Fri Mar 07 2025 Devrim Gunduz <devrim@gunduz.org> - 15.12-2PGDG
 - Remove redundant BR
 

commit 6e46ff1d62450ad76e64a429db96ae367ef56ec1

PostgreSQL 16, 17 and 18: Add explicit calls to CLANG and LLVM_CONFIG back to fix extension builds. I broke it in c041426cd5 .Per report from Muralikrishna Bandaru and  https://www.postgresql.org/message-id/CACMiCkV%2BfQ4yAZqygyWx7ZQ8eWsj1AjoC6CGEUoyxY9jUm7paA%40mail.gmail.com

rpm/redhat/main/non-common/postgresql-16/main/postgresql-16.spec
@@ -58,9 +58,9 @@ Version:	16.9
 %if 0%{?suse_version} >= 1315
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	420001PGDG%{?dist}
+Release:	420002PGDG%{?dist}
 %else
-Release:	1PGDG%{?dist}
+Release:	2PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -477,6 +477,13 @@ LDFLAGS="-Wl,--as-needed"; export LDFLAGS
 %endif
 
 export CFLAGS
+
+# We need to export these even though they are under the standard
+# path. Buildfarm utilises ccache which may not be available on
+# users' instances, and that breaks extension builds as shown here:
+# https://www.postgresql.org/message-id/CACMiCkV%2BfQ4yAZqygyWx7ZQ8eWsj1AjoC6CGEUoyxY9jUm7paA%40mail.gmail.com
+# Previously reported by Muralikrishna Bandaru.
+export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
 
 # These configure options must match main build
 ./configure --enable-rpath \
@@ -1227,6 +1234,11 @@ fi
 %endif
 
 %changelog
+* Tue May 13 2025 Devrim Gunduz <devrim@gunduz.org> - 16.9-2PGDG
+- Add explicit calls to CLANG and LLVM_CONFIG back to fix extension
+  builds. Per report from Muralikrishna Bandaru and
+  https://www.postgresql.org/message-id/CACMiCkV%2BfQ4yAZqygyWx7ZQ8eWsj1AjoC6CGEUoyxY9jUm7paA%40mail.gmail.com
+
 * Tue May 6 2025 Devrim Gunduz <devrim@gunduz.org> - 16.9-1PGDG
 - Update to 16.9, per changes described at:
   https://www.postgresql.org/docs/release/16.9/

rpm/redhat/main/non-common/postgresql-17/main/postgresql-17.spec
@@ -1,3 +1,4 @@
+%global debug_package %{nil}
 %undefine _package_note_file
 
 # These are macros to be used with find_lang and other stuff
@@ -43,9 +44,9 @@ Version:	17.5
 %if 0%{?suse_version} >= 1500
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	420001PGDG%{?dist}
+Release:	420002PGDG%{?dist}
 %else
-Release:	1PGDG%{?dist}
+Release:	2PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -450,6 +451,13 @@ CFLAGS=`echo $CFLAGS|xargs -n 1|grep -v ffast-math|xargs -n 100`
 LDFLAGS="-Wl,--as-needed"; export LDFLAGS
 %endif
 export CFLAGS
+
+# We need to export these even though they are under the standard
+# path. Buildfarm utilises ccache which may not be available on
+# users' instances, and that breaks extension builds as shown here:
+# https://www.postgresql.org/message-id/CACMiCkV%2BfQ4yAZqygyWx7ZQ8eWsj1AjoC6CGEUoyxY9jUm7paA%40mail.gmail.com
+# Previously reported by Muralikrishna Bandaru.
+export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
 
 # These configure options must match main build
 ./configure --enable-rpath \
@@ -1214,6 +1222,11 @@ fi
 %endif
 
 %changelog
+* Tue May 13 2025 Devrim Gunduz <devrim@gunduz.org> - 17.5-2PGDG
+- Add explicit calls to CLANG and LLVM_CONFIG back to fix extension
+  builds. Per report from Muralikrishna Bandaru and
+  https://www.postgresql.org/message-id/CACMiCkV%2BfQ4yAZqygyWx7ZQ8eWsj1AjoC6CGEUoyxY9jUm7paA%40mail.gmail.com
+
 * Tue May 6 2025 Devrim Gündüz <devrim@gunduz.org> - 17.5-1PGDG
 - Update to 17.5 per changes described at:
   https://www.postgresql.org/docs/release/17.5/

rpm/redhat/main/non-common/postgresql-18/main/postgresql-18.spec
@@ -45,9 +45,9 @@ Version:	18
 %if 0%{?suse_version} >= 1500
 # SuSE upstream packages have release numbers like 150200.5.19.1
 # which overrides our packages. Increase our release number on SuSE.
-Release:	beta1_PGDG%{?dist}
+Release:	beta1_4200002PGDG%{?dist}
 %else
-Release:	beta1_PGDG%{?dist}
+Release:	beta1_2PGDG%{?dist}
 %endif
 License:	PostgreSQL
 Url:		https://www.postgresql.org/
@@ -505,6 +505,13 @@ LDFLAGS="-Wl,--as-needed"; export LDFLAGS
 %endif
 
 export CFLAGS
+
+# We need to export these even though they are under the standard
+# path. Buildfarm utilises ccache which may not be available on
+# users' instances, and that breaks extension builds as shown here:
+# https://www.postgresql.org/message-id/CACMiCkV%2BfQ4yAZqygyWx7ZQ8eWsj1AjoC6CGEUoyxY9jUm7paA%40mail.gmail.com
+# Previously reported by Muralikrishna Bandaru.
+export CLANG=%{_bindir}/clang LLVM_CONFIG=%{_bindir}/llvm-config
 
 ./configure --enable-rpath \
 	--prefix=%{pgbaseinstdir} \
@@ -1297,6 +1304,11 @@ fi
 %endif
 
 %changelog
+* Tue May 13 2025 Devrim Gunduz <devrim@gunduz.org> - 18.0beta1-2PGDG
+- Add explicit calls to CLANG and LLVM_CONFIG back to fix extension
+  builds. Per report from Muralikrishna Bandaru and
+  https://www.postgresql.org/message-id/CACMiCkV%2BfQ4yAZqygyWx7ZQ8eWsj1AjoC6CGEUoyxY9jUm7paA%40mail.gmail.com
+
 * Tue May 6 2025 Devrim Gunduz <devrim@gunduz.org> - 18.0beta1-1PGDG
 - Update to PostgreSQL 18 beta1
 