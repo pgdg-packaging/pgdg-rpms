860ccbb8ae873fe6ddef022189529375a931a7f7
Devrim Gunduz <devrim@gunduz.org>
2025-07-10 14:02:59 +0300

    scripts:  We currently pull packages from yonada.

--- scripts/buildserver/packagesyncnonfree.sh
+++ scripts/buildserver/packagesyncnonfree.sh
@@ -77,10 +77,10 @@ do
 	echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $DEBUG_RPM_DIR/repodata/repomd.xml
 	echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $SRPM_DIR/repodata/repomd.xml
 
-	# Finally, perform the rsync:
-	rsync --checksum -ave ssh --delete $RPM_DIR/ yumupload@yum.postgresql.org:yum/yum/non-free/$packageSyncVersion/$osdistro/$os-$osarch
-	rsync --checksum -ave ssh --delete $DEBUG_RPM_DIR/ yumupload@yum.postgresql.org:yum/yum/non-free/debug/$packageSyncVersion/$osdistro/$os-$osarch
-	rsync --checksum -ave ssh --delete $SRPM_DIR/ yumupload@yum.postgresql.org:yum/yum/srpms/non-free/$packageSyncVersion/$osdistro/$os-$osarch
+	# We currently pull packages from yonada, so skip the next line:
+	# rsync --checksum -ave ssh --delete $RPM_DIR/ yumupload@yum.postgresql.org:yum/yum/non-free/$packageSyncVersion/$osdistro/$os-$osarch
+	# rsync --checksum -ave ssh --delete $DEBUG_RPM_DIR/ yumupload@yum.postgresql.org:yum/yum/non-free/debug/$packageSyncVersion/$osdistro/$os-$osarch
+	# rsync --checksum -ave ssh --delete $SRPM_DIR/ yumupload@yum.postgresql.org:yum/yum/srpms/non-free/$packageSyncVersion/$osdistro/$os-$osarch
 
 	# Sync SRPMs to S3 bucket:
 	aws s3 sync $SRPM_DIR s3://dnf-srpms.postgresql.org20250313103537584600000001/srpms/non-free/$packageSyncVersion/$osdistro/$os-$osarch --exclude "*.html"
