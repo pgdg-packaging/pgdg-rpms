684ca24911325be797ad6e3d729faa4a6f04799a
Devrim Gunduz <devrim@gunduz.org>
2025-10-01 18:20:47 +0100

    scripts/packagesync.sh: Fix invalidation path

--- scripts/buildserver/packagesync.sh
+++ scripts/buildserver/packagesync.sh
@@ -63,7 +63,7 @@ do
 	# Sync SRPMs to S3 bucket:
 	aws s3 sync $SRPM_DIR s3://dnf-srpms.postgresql.org20250313103537584600000001/srpms/$packageSyncVersion/$osdistro/$os-$osarch --exclude "*.html" --exclude "repodata"
 	aws s3 sync --delete $SRPM_DIR/repodata/ s3://dnf-srpms.postgresql.org20250313103537584600000001/srpms/$packageSyncVersion/$osdistro/$os-$osarch/repodata/ --exclude "*.html"
-	aws cloudfront create-invalidation --distribution-id $CF_SRPM_DISTRO_ID --path /debug/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
+	aws cloudfront create-invalidation --distribution-id $CF_SRPM_DISTRO_ID --path /srpms/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
 
 	# Sync debug* RPMs to S3 bucket:
 	aws s3 sync $DEBUG_RPM_DIR s3://dnf-debuginfo.postgresql.org20250312201116649700000001/debug/$packageSyncVersion/$osdistro/$os-$osarch/ --exclude "*.html" --exclude "repodata"
