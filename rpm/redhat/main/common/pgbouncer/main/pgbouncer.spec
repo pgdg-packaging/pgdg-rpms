pgdg-packaging/pgdg-rpms

commit ec52b3842229d531ca2e6fc08b412cd34374cba1

pgbouncer: Add sysusers.d config file to allow rpm to create users/groups automatically. While passing, add c-ares support to SLES 15 as well.

rpm/redhat/main/common/pgbouncer/main/pgbouncer.spec
@@ -1,6 +1,6 @@
 Name:		pgbouncer
 Version:	1.24.1
-Release:	42PGDG%{?dist}
+Release:	43PGDG%{?dist}
 Summary:	Lightweight connection pooler for PostgreSQL
 License:	MIT and BSD
 URL:		https://www.pgbouncer.org/
@@ -8,6 +8,7 @@ Source0:	https://www.pgbouncer.org/downloads/files/%{version}/%{name}-%{version}
 Source2:	%{name}.sysconfig
 Source3:	%{name}.logrotate
 Source4:	%{name}.service
+Source5:	%{name}-sysusers.conf
 Patch0:		%{name}-ini.patch
 
 Requires:	python3 python3-psycopg2
@@ -17,16 +18,20 @@ Requires:	libevent >= 2.0
 
 BuildRequires:	openssl-devel pam-devel
 
-%if 0%{?fedora} >= 40 || 0%{?rhel} >= 9
+%if 0%{?fedora} >= 41 || 0%{?rhel} >= 9
 BuildRequires:	c-ares-devel >= 1.13
 Requires:	c-ares >= 1.13
+%endif
+%if 0%{?suse_version} >= 1500
+BuildRequires:	c-ares-devel >= 1.13
+Requires:	libcares2 >= 1.19
 %endif
 
 BuildRequires:		systemd
 # We require this to be present for %%{_prefix}/lib/tmpfiles.d
 Requires:		systemd
 %if 0%{?suse_version}
-%if 0%{?suse_version} >= 1315
+%if 0%{?suse_version} >= 1500
 Requires(post):		systemd-sysvinit
 %endif
 %else
@@ -35,8 +40,6 @@ Requires(post):		systemd
 Requires(preun):	systemd
 Requires(postun):	systemd
 %endif
-
-Requires:	/usr/sbin/useradd
 
 %description
 pgbouncer is a lightweight connection pooler for PostgreSQL.
@@ -52,19 +55,16 @@ sed -i.fedora \
  -e '/BININSTALL/s|-s||' \
  configure
 
-# c-ares >= 1.16 is needed for proper c-ares support. Currently only RHEL 9
-# and Fedora has it. Use libevent on the remaining ones.
+# c-ares >= 1.16 is needed for proper c-ares support. Currently
+# only RHEL 8 does not have it, so use libevent only on RHEL 8.
 # Per https://redmine.postgresql.org/issues/6315
-
 %configure \
 	--datadir=%{_datadir} \
-%if 0%{?fedora} >= 40 || 0%{?rhel} >= 9
-	--with-cares --disable-evdns \
-%else
+%if 0%{?rhel} <= 8
 	--without-cares \
+%else
+	--with-cares --disable-evdns \
 %endif
-	--with-systemd \
-	--with-pam
 
 %{__make} %{?_smp_mflags} V=1
 
@@ -87,6 +87,8 @@ cat > %{buildroot}%{_tmpfilesdir}/%{name}.conf <<EOF
 d %{_rundir}/%{name} 0700 pgbouncer pgbouncer -
 EOF
 
+# Install sysusers.d config file to allow rpm to create users/groups automatically.
+%{__install} -m 0644 -D %{SOURCE5} %{buildroot}%{_sysusersdir}/%{name}-pgdg.conf
 
 # Create the directory for the pid files (defined in pgbouncer.ini)
 %{__install} -d -m 755 %{buildroot}/var/run/%{name}
@@ -132,6 +134,7 @@ fi
 %config(noreplace) %{_sysconfdir}/%{name}/%{name}.ini
 %ghost %{_rundir}/%{name}
 %{_tmpfilesdir}/%{name}.conf
+%{_sysusersdir}/%{name}-pgdg.conf
 %attr(644,root,root) %{_unitdir}/%{name}.service
 %config(noreplace) %{_sysconfdir}/sysconfig/%{name}
 %config(noreplace) %{_sysconfdir}/logrotate.d/%{name}
@@ -141,6 +144,10 @@ fi
 %attr(755,pgbouncer,pgbouncer) %dir /var/run/%{name}
 
 %changelog
+* Mon Sep 22 2025 Devrim Gündüz <devrim@gunduz.org> - 1.24.1-43PGDG
+- Add sysusers.d config file to allow rpm to create users/groups automatically.
+- Add c-ares support to SLES 15 as well.
+
 * Wed Apr 16 2025 Devrim Gündüz <devrim@gunduz.org> - 1.24.1-42PGDG
 - Update to 1.24.1, per changes described at:
   http://www.pgbouncer.org/changelog.html#pgbouncer-124x

rpm/redhat/main/common/pgbouncer/EL-10/pgbouncer-sysusers.conf
@@ -1,0 +1,1 @@
+../main/pgbouncer-sysusers.conf

rpm/redhat/main/common/pgbouncer/EL-8/pgbouncer-sysusers.conf
@@ -1,0 +1,1 @@
+../main/pgbouncer-sysusers.conf

rpm/redhat/main/common/pgbouncer/EL-9/pgbouncer-sysusers.conf
@@ -1,0 +1,1 @@
+../main/pgbouncer-sysusers.conf

rpm/redhat/main/common/pgbouncer/F-41/pgbouncer-sysusers.conf
@@ -1,0 +1,1 @@
+../main/pgbouncer-sysusers.conf

rpm/redhat/main/common/pgbouncer/F-42/pgbouncer-sysusers.conf
@@ -1,0 +1,1 @@
+../main/pgbouncer-sysusers.conf

rpm/redhat/main/common/pgbouncer/F-43/pgbouncer-sysusers.conf
@@ -1,0 +1,1 @@
+../main/pgbouncer-sysusers.conf

rpm/redhat/main/common/pgbouncer/SLES-15/pgbouncer-sysusers.conf
@@ -1,0 +1,1 @@
+../main/pgbouncer-sysusers.conf

commit 0d29d7ee31aad70f875326cbcb536f9f9cf41153

pgbouncer: Re-add systemd and pam support that I broke in ec52b384. Fixes https://github.com/pgbouncer/pgbouncer/issues/1416

rpm/redhat/main/common/pgbouncer/main/pgbouncer.spec
@@ -1,6 +1,6 @@
 Name:		pgbouncer
 Version:	1.25.0
-Release:	43PGDG%{?dist}
+Release:	44PGDG%{?dist}
 Summary:	Lightweight connection pooler for PostgreSQL
 License:	MIT and BSD
 URL:		https://www.pgbouncer.org/
@@ -77,6 +77,8 @@ sed -i.fedora \
 %else
 	--with-cares --disable-evdns \
 %endif
+	--with-systemd \
+	--with-pam
 
 %{__make} %{?_smp_mflags} V=1
 
@@ -151,6 +153,10 @@ fi
 %attr(755,pgbouncer,pgbouncer) %dir /var/run/%{name}
 
 %changelog
+* Wed Nov 12 2025 Devrim Gündüz <devrim@gunduz.org> - 1.25.0-44PGDG
+- Re-add systemd and pam support that I broke in ec52b384. Fixes
+  https://github.com/pgbouncer/pgbouncer/issues/1416
+
 * Mon Nov 10 2025 Devrim Gündüz <devrim@gunduz.org> - 1.25.0-43PGDG
 - Add a patch from upstream to fix ppc64le builds, per:
   https://github.com/pgbouncer/pgbouncer/issues/1413