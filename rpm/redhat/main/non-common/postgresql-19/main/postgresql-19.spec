pgdg-packaging/pgdg-rpms

commit ebc40a17a38638a7fb1c0a1219e46cee31eb2b09

PostgreSQL 19: Add pg_plan_advice. Also install pg_regress.so under $libdir as a part of https://github.com/pgdg-packaging/pgdg-rpms/issues/165

rpm/redhat/main/non-common/postgresql-19/main/postgresql-19.spec
@@ -759,6 +759,7 @@ touch -r %{SOURCE10} %{sname}-%{pgmajorversion}-check-db-dir
 	strip *.so
 	%{__rm} -f GNUmakefile Makefile *.o
 	chmod 0755 pg_regress regress.so
+	cp regress.so  %{buildroot}%{pgbaseinstdir}/lib/regress.so
 	popd
 	%{__cp} %{SOURCE4} %{buildroot}%{pgbaseinstdir}/lib/test/regress/Makefile
 	chmod 0644 %{buildroot}%{pgbaseinstdir}/lib/test/regress/Makefile
@@ -1052,6 +1053,7 @@ fi
 %{pgbaseinstdir}/lib/pg_freespacemap.so
 %{pgbaseinstdir}/lib/pg_logicalinspect.so
 %{pgbaseinstdir}/lib/pg_overexplain.so
+%{pgbaseinstdir}/lib/pg_plan_advice.so
 %{pgbaseinstdir}/lib/pg_prewarm.so
 %{pgbaseinstdir}/lib/pg_stat_statements.so
 %{pgbaseinstdir}/lib/pg_surgery.so
@@ -1313,6 +1315,7 @@ fi
 %defattr(-,postgres,postgres)
 %attr(-,postgres,postgres) %{pgbaseinstdir}/lib/test/*
 %attr(-,postgres,postgres) %dir %{pgbaseinstdir}/lib/test
+%{pgbaseinstdir}/lib/regress.so
 %endif
 
 %changelog

commit 5e6ea948efb1d3ac8c7fc991e19a253abbcae125

PostgreSQL 19: Fix "Duplicate build-ids" issue that I introduced in ebc40a17a3, which was a part of https://github.com/pgdg-packaging/pgdg-rpms/issues/165

rpm/redhat/main/non-common/postgresql-19/main/postgresql-19.spec
@@ -768,7 +768,8 @@ touch -r %{SOURCE10} %{sname}-%{pgmajorversion}-check-db-dir
 	strip *.so
 	%{__rm} -f GNUmakefile Makefile *.o
 	chmod 0755 pg_regress regress.so
-	cp regress.so  %{buildroot}%{pgbaseinstdir}/lib/regress.so
+	%{__ln} %{buildroot}%{pgbaseinstdir}/lib/test/regress/regress.so \
+		%{buildroot}%{pgbaseinstdir}/lib/regress.so
 	popd
 	%{__cp} %{SOURCE4} %{buildroot}%{pgbaseinstdir}/lib/test/regress/Makefile
 	chmod 0644 %{buildroot}%{pgbaseinstdir}/lib/test/regress/Makefile