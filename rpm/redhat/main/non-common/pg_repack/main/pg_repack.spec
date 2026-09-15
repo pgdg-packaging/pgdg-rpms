pgdg-packaging/pgdg-rpms

commit 000c2b6eeb3333c071ff46ad34f6c55de52b0fcc

Various packages: Add missing BRs. Per https://github.com/pgdg-packaging/pgdg-rpms/issues/237 . This batch ignored "older" GeOS, GDAL and PostGIS packages along with a few more packages that have more problems.

rpm/redhat/main/non-common/pg_repack/main/pg_repack.spec
@@ -16,11 +16,12 @@
 Summary:	Reorganize tables in PostgreSQL databases without any locks
 Name:		%{sname}_%{pgmajorversion}
 Version:	1.5.3
-Release:	6PGDG%{?dist}
+Release:	7PGDG%{?dist}
 License:	BSD
 Source0:	https://github.com/reorg/%{sname}/archive/refs/tags/ver_%{version}.tar.gz
 URL:		https://github.com/reorg/%{sname}/
 
+BuildRequires:	numactl-devel
 BuildRequires:	postgresql%{pgmajorversion}-devel postgresql%{pgmajorversion}
 BuildRequires:	readline-devel zlib-devel
 # lz4 dependency
@@ -112,6 +113,9 @@ USE_PGXS=1 PATH=%{pginstdir}/bin/:$PATH %{__make} %{with_llvm_arg} DESTDIR=%{bui
 %endif
 
 %changelog
+* Thu Sep 10 2026 Devrim Gunduz <devrim@gunduz.org> - 1.5.3-7PGDG
+- Add missing BR
+
 * Sun Aug 30 2026 Devrim Gunduz <devrim@gunduz.org> - 1.5.3-6PGDG
 - Make %%llvm actually control the build, not just packaging: pass
   with_llvm=no to make when %%llvm is 0, otherwise setting %%llvm 0 only

commit 1e12206083bc86e97cb6abb2c4949eee31f054ec

pg_repack: Fix numactl BR that I broke in 000c2b6ee. Not bumping up the release number as I did not build this release yet.

rpm/redhat/main/non-common/pg_repack/main/pg_repack.spec
@@ -21,7 +21,13 @@ License:	BSD
 Source0:	https://github.com/reorg/%{sname}/archive/refs/tags/ver_%{version}.tar.gz
 URL:		https://github.com/reorg/%{sname}/
 
+%if 0%{?rhel} || 0%{?fedora}
 BuildRequires:	numactl-devel
+Requires:	numactl-libs
+%else
+BuildRequires:	libnuma-devel
+Requires:	libnuma1
+%endif
 BuildRequires:	postgresql%{pgmajorversion}-devel postgresql%{pgmajorversion}
 BuildRequires:	readline-devel zlib-devel
 # lz4 dependency