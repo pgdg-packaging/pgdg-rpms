pgdg-packaging/pgdg-rpms

commit 000c2b6eeb3333c071ff46ad34f6c55de52b0fcc

Various packages: Add missing BRs. Per https://github.com/pgdg-packaging/pgdg-rpms/issues/237 . This batch ignored "older" GeOS, GDAL and PostGIS packages along with a few more packages that have more problems.

rpm/redhat/main/non-common/pgimportdoc/main/pgimportdoc.spec
@@ -3,10 +3,11 @@
 Summary:	command line tool for import XML, TEXT and BYTEA documents to PostgreSQL
 Name:		%{sname}_%{pgmajorversion}
 Version:	0.1.4
-Release:	6PGDG%{?dist}
+Release:	7PGDG%{?dist}
 License:	BSD
 Source0:	https://github.com/okbob/%{sname}/archive/%{version}.tar.gz
 URL:		https://github.com/okbob/%{sname}
+BuildRequires:	clang gcc make numactl-devel
 BuildRequires:	postgresql%{pgmajorversion}-devel postgresql%{pgmajorversion}
 # All supported distros have libselinux-devel package:
 BuildRequires:	libselinux-devel >= 2.0.93
@@ -72,6 +73,9 @@ USE_PGXS=1 PATH=%{pginstdir}/bin/:$PATH %{__make} %{?_smp_mflags} DESTDIR=%{buil
 %{pginstdir}/bin/%{sname}
 
 %changelog
+* Thu Sep 10 2026 Devrim Gündüz <devrim@gunduz.org> - 0.1.4-7PGDG
+- Add missing BR
+
 * Mon Aug 24 2026 Devrim Gündüz <devrim@gunduz.org> - 0.1.4-6PGDG
 - Fix OpenSSL dependency for Amazon Linux 2023
 

commit 64df73df3b3ed7ed8354b9a6ce0b70029f8d6c22

pgimportdoc: Fix numactl BR that I broke in 000c2b6ee. Not bumping up the release number as I did not build this release yet.

rpm/redhat/main/non-common/pgimportdoc/main/pgimportdoc.spec
@@ -7,7 +7,7 @@ Release:	7PGDG%{?dist}
 License:	BSD
 Source0:	https://github.com/okbob/%{sname}/archive/%{version}.tar.gz
 URL:		https://github.com/okbob/%{sname}
-BuildRequires:	clang gcc make numactl-devel
+BuildRequires:	clang gcc make
 BuildRequires:	postgresql%{pgmajorversion}-devel postgresql%{pgmajorversion}
 # All supported distros have libselinux-devel package:
 BuildRequires:	libselinux-devel >= 2.0.93
@@ -24,6 +24,13 @@ Requires:	liblz4-1
 %if 0%{?rhel} || 0%{?fedora}
 BuildRequires:	lz4-devel
 Requires:	lz4-libs
+%endif
+%if 0%{?rhel} || 0%{?fedora}
+BuildRequires:  numactl-devel
+Requires:	numactl-libs
+%else
+BuildRequires:	libnuma-devel
+Requires:	libnuma1
 %endif
 # zstd dependency
 %if 0%{?suse_version} >= 1500