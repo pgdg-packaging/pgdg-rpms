pgdg-packaging/pgdg-rpms

commit 000c2b6eeb3333c071ff46ad34f6c55de52b0fcc

Various packages: Add missing BRs. Per https://github.com/pgdg-packaging/pgdg-rpms/issues/237 . This batch ignored "older" GeOS, GDAL and PostGIS packages along with a few more packages that have more problems.

rpm/redhat/main/non-common/pg_catcheck/main/pg_catcheck.spec
@@ -3,11 +3,12 @@
 Summary:	Tool for diagnosing PostgreSQL system catalog corruption
 Name:		%{sname}_%{pgmajorversion}
 Version:	1.6.0
-Release:	5PGDG%{?dist}
+Release:	6PGDG%{?dist}
 License:	BSD
 Source0:	https://github.com/EnterpriseDB/%{sname}/archive/%{version}.tar.gz
 URL:		https://github.com/EnterpriseDB/%{sname}
-BuildRequires:	postgresql%{pgmajorversion}-devel
+BuildRequires:	clang gcc make
+BuildRequires:	postgresql%{pgmajorversion}-devel numactl-devel
 Requires:	postgresql%{pgmajorversion}-server
 # All supported distros have libselinux-devel package:
 BuildRequires:	libselinux-devel >= 2.0.93
@@ -82,6 +83,9 @@ USE_PGXS=1 PATH=%{pginstdir}/bin/:$PATH %{__make} %{?_smp_mflags} install DESTDI
 %{pginstdir}/bin/%{sname}
 
 %changelog
+* Thu Sep 10 2026 Devrim Gündüz <devrim@gunduz.org> - 1.6.0-6PGDG
+- Add missing BR
+
 * Mon Aug 24 2026 Devrim Gündüz <devrim@gunduz.org> - 1.6.0-5PGDG
 - Fix OpenSSL dependency for Amazon Linux 2023
 

commit 00f6e338d12ef265de7c70bc0417ea72d9966fb5

pg_catcheck: Fix numactl BR that I broke in 000c2b6ee. Not bumping up the release number as I did not build this release yet.

rpm/redhat/main/non-common/pg_catcheck/main/pg_catcheck.spec
@@ -8,7 +8,7 @@ License:	BSD
 Source0:	https://github.com/EnterpriseDB/%{sname}/archive/%{version}.tar.gz
 URL:		https://github.com/EnterpriseDB/%{sname}
 BuildRequires:	clang gcc make
-BuildRequires:	postgresql%{pgmajorversion}-devel numactl-devel
+BuildRequires:	postgresql%{pgmajorversion}-devell
 Requires:	postgresql%{pgmajorversion}-server
 # All supported distros have libselinux-devel package:
 BuildRequires:	libselinux-devel >= 2.0.93
@@ -41,7 +41,14 @@ BuildRequires:	krb5-devel readline-devel zlib-devel
 Requires:	libopenssl3
 BuildRequires:	libopenssl-3-devel
 %endif
-%if 0%{?fedora} >= 41 || 0%{?rhel} >= 8 || 0%{?amzn}
+%if 0%{?rhel} || 0%{?fedora}
+BuildRequires:	numactl-devel
+Requires:	numactl-libs
+%else
+BuildRequires:	libnuma-devel
+Requires:	libnuma1
+%endif
+%if 0%{?fedora} >= 43 || 0%{?rhel} >= 8 || 0%{?amzn}
 Requires:	openssl-libs >= 1.1.1k
 BuildRequires:	openssl-devel
 %endif