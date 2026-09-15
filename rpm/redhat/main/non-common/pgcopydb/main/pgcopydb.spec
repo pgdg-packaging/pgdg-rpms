pgdg-packaging/pgdg-rpms

commit 000c2b6eeb3333c071ff46ad34f6c55de52b0fcc

Various packages: Add missing BRs. Per https://github.com/pgdg-packaging/pgdg-rpms/issues/237 . This batch ignored "older" GeOS, GDAL and PostGIS packages along with a few more packages that have more problems.

rpm/redhat/main/non-common/pgcopydb/main/pgcopydb.spec
@@ -4,11 +4,12 @@
 Summary:	Automate pg_dump | pg_restore between two running Postgres servers
 Name:		%{sname}
 Version:	0.18
-Release:	2PGDG%{?dist}
+Release:	3PGDG%{?dist}
 License:	PostgreSQL
 Source0:	https://github.com/dimitri/%{sname}/archive/refs/tags/v%{version}.tar.gz
 URL:		https://github.com/dimitri/%{sname}
-BuildRequires:	postgresql%{pgmajorversion}-devel gc-devel
+BuildRequires:	gcc make
+BuildRequires:	postgresql%{pgmajorversion}-devel gc-devel numactl-devel
 BuildRequires:	krb5-devel bison flex sqlite-devel
 # zstd dependency
 %if 0%{?suse_version} >= 1500
@@ -74,6 +75,9 @@ USE_PGXS=1 PATH=%{pginstdir}/bin:$PATH %{__make} %{?_smp_mflags} DESTDIR=%{build
 %{pginstdir}/bin/pgcopydb
 
 %changelog
+* Thu Sep 10 2026 Devrim Gündüz <devrim@gunduz.org> - 0.18-3PGDG
+- Add missing BR
+
 * Mon Aug 24 2026 Devrim Gündüz <devrim@gunduz.org> - 0.18-2PGDG
 - Fix OpenSSL dependency for Amazon Linux 2023
 

commit ec807932429401f23fa92787b920097df1c9182d

pgcopydb: Fix numactl BR that I broke in 000c2b6ee. Not bumping up the release number as I did not build this release yet.

rpm/redhat/main/non-common/pgcopydb/main/pgcopydb.spec
@@ -9,7 +9,7 @@ License:	PostgreSQL
 Source0:	https://github.com/dimitri/%{sname}/archive/refs/tags/v%{version}.tar.gz
 URL:		https://github.com/dimitri/%{sname}
 BuildRequires:	gcc make
-BuildRequires:	postgresql%{pgmajorversion}-devel gc-devel numactl-devel
+BuildRequires:	postgresql%{pgmajorversion}-devel gc-devel
 BuildRequires:	krb5-devel bison flex sqlite-devel
 # zstd dependency
 %if 0%{?suse_version} >= 1500
@@ -34,13 +34,20 @@ Requires:	lz4-libs
 Requires:	libopenssl3
 BuildRequires:	libopenssl-3-devel
 %endif
-%if 0%{?fedora} >= 41 || 0%{?rhel} >= 8 || 0%{?amzn}
+%if 0%{?rhel} || 0%{?fedora}
+BuildRequires:	numactl-devel
+Requires:	numactl-libs
+%else
+BuildRequires:	libnuma-devel
+Requires:	libnuma1
+%endif
+%if 0%{?fedora} >= 43 || 0%{?rhel} >= 8 || 0%{?amzn}
 Requires:	openssl-libs >= 1.1.1k
 BuildRequires:	openssl-devel
 %endif
 BuildRequires:	libxml2-devel libxslt-devel pam-devel
 BuildRequires:	readline-devel zlib-devel
-%if 0%{?fedora} >= 40 || 0%{?rhel} >= 8
+%if 0%{?fedora} >= 43 || 0%{?rhel} >= 8
 Requires:	gc
 %endif
 %if 0%{?suse_version} >= 1500